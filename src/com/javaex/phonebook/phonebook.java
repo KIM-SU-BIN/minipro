20c195f7b579313e9f89a925839cd00827198835
subintoto <subintoto@naver.com>
2022-05-11 00:31:27 +0900

    phonebook CL

--- src/com/javaex/phonebook/phonebook.java
+++ src/com/javaex/phonebook/phonebook.java
@@ -40,27 +40,19 @@ public class phonebook {
 			pList.add(person);
 		}
 
-		
-		// 초기값
-		//int now = 0;
-		//String inputLine;
-		//String[] inputArray;
-		
-
 		// 초기화면
-		while (true) {
-			System.out.println("");
-			System.out.println("************************");
-			System.out.println("*      전화번호 관리 프로그램      *");
-			System.out.println("************************");
+		System.out.println("");
+		System.out.println("************************");
+		System.out.println("*      전화번호 관리 프로그램      *");
+		System.out.println("************************");
 
+		while (true) {
 			System.out.println("");
 			System.out.println(" 1.리스트 | 2.등록 | 3.삭제 | 4.검색 | 5.종료");
 			System.out.println("----------------------------------");
 			System.out.println("> 메뉴번호 : ");
 			int n = sc.nextInt();
 
-			
 			// 종료
 			if (n == 5) {
 				System.out.println("***********************");
@@ -68,25 +60,23 @@ public class phonebook {
 				System.out.println("***********************");
 				break;
 
-				
-			// 리스트 프로그램 시작
+				// 리스트 프로그램 시작
 			} else if (n == 1) {
-				System.out.println("< 리스트 >");
+				System.out.println("< 1.리스트 >");
 				System.out.println("");
-				//List<Person> personList = new ArrayList<Person>();
+				// List<Person> personList = new ArrayList<Person>();
 
-				
 				// 출력하기
 				int i = 1;
 				for (Person person : pList) {
-					System.out.println((i) + ". \t" + person.getName() + "\t" + person.getHp() + "\t" + person.getCompany());
+					System.out.println(
+							(i) + ". \t" + person.getName() + "\t" + person.getHp() + "\t" + person.getCompany());
 					i++;
 				}
 
-				
 				// 등록 프로그램 시작
 			} else if (n == 2) {
-				
+
 				// 읽기스트림
 				Writer fw = new FileWriter("C:\\javaStudy\\미니프로젝트\\PhoneDB.txt");
 				BufferedWriter bw = new BufferedWriter(fw);
@@ -101,51 +91,58 @@ public class phonebook {
 				System.out.println(">회사전화 : ");
 				String company = sc.next();
 
-				Person newperson = new Person(name , hp ,company);
+				Person newperson = new Person(name, hp, company);
 				pList.add(newperson);
-				
-				for (Person person: pList) {
-					//inputLine = sc.nextLine();
-					//inputArray = inputLine.split(" ");
-					
-					String plus = person.getName() + "," +person.getHp() +","+person.getCompany();
-					
+
+				for (Person person : pList) {
+					// inputLine = sc.nextLine();
+					// inputArray = inputLine.split(" ");
+
+					String plus = person.getName() + "," + person.getHp() + "," + person.getCompany();
+
 					bw.write(plus);
 					bw.newLine();
-					
+
 				}
 				System.out.println("등록되었습니다.");
 				bw.flush();
-				
+				bw.close();
 
 				// 삭제 프로그램 시작
 			} else if (n == 3) {
+				
+				// 읽기스트림
+				Writer fw = new FileWriter("C:\\javaStudy\\미니프로젝트\\PhoneDB.txt");
+				BufferedWriter bw = new BufferedWriter(fw);
+
+
 				System.out.println("<3. 삭제>");
 				System.out.println("> 번호 : ");
 				int minus = sc.nextInt() - 1;
 				pList.remove(minus);
 
 				System.out.println("삭제되었습니다.");
-				
-				
-				//검색 프로그램 시작
-			} else if (n ==4) {
+				bw.flush();
+				bw.close();
+
+				// 검색 프로그램 시작
+			} else if (n == 4) {
 				System.out.println("<4.검색>");
 				System.out.println(">이름 : ");
-				String  serch = sc.next();
-				
-				for (Person person : pList) {
-					if(person.getName().contains(serch))
-					System.out.println(person.toString());
-				}
-				
-			//없는메뉴
-			} else {
-				 System.out.println("[다시 입력해 주세요.]");
+				String serch = sc.next();
 
+				for (Person person : pList) {
+					if (person.getName().contains(serch))
+						System.out.println(person.toString());
+				}
+
+				// 없는메뉴
+			} else {
+				System.out.println("[다시 입력해 주세요.]");
+			}
 		}
 		isr.close();
 		sc.close();
-			}
-		}
+
 	}
+}
